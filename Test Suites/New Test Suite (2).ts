8748e20029d64d03f15e43a5e00872bece5740f0
Lam (Henry) Do <lam.do@katalon.com>
2024-08-19 18:11:46 +0700

    test

---
+++ Test Suites/New Test Suite (2).ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite (2)</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b0f93d82-7460-4f72-9c07-8b06940cab13</testSuiteGuid>
+</TestSuiteEntity>
